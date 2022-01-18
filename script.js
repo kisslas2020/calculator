6948e6d56f80704554632d77bd30314c275aa920
Laszlo Kiss <pinewood82@gmail.com>
2022-01-18 15:46:34 +0100

    Add eventlisteners to buttons

--- script.js
+++ script.js
@@ -1,3 +1,54 @@
+let op = null;
+let num1 = null;
+let num2 = null;
+let displayValue = '';
+
+const display = document.querySelector('.display');
+const numbers = document.querySelectorAll('.numbers button');
+numbers.forEach(n => n.addEventListener('click', () => {
+    if (displayValue != 'error') {
+        displayValue += n.textContent;
+        display.textContent = displayValue;
+    }
+}));
+
+const operators = document.querySelectorAll('.operators button');
+operators.forEach(o => o.addEventListener('click', () => {
+    if (displayValue != 'error') {
+        if (num1 === null) {
+            num1 = displayValue;
+            displayValue = '';
+        } else if (num1 != displayValue) {
+            num1 = operate(op, num1, displayValue);
+            displayValue = num1 === null ? 'error' : num1;
+            display.textContent = displayValue;
+        }
+        op = o.textContent;
+    }
+}));
+
+const equalsButton = document.querySelector('#equals');
+equalsButton.addEventListener('click', () => {
+    if (displayValue != 'error') {
+        num2 = displayValue;
+        if (num1 === null || num1 === '') {
+            num1 = 0;
+        }
+        num1 = operate(op, num1, num2);
+        displayValue = num1 === null ? 'error' : num1;
+        display.textContent = displayValue;
+    }
+});
+
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', () => {
+    op = null;
+    num1 = null;
+    num2 = null;
+    displayValue = '';
+    display.textContent = '0';
+})
+
 function operate(operator, n1, n2) {
     switch(operator) {
         case '+':
@@ -42,7 +93,7 @@ function divide(a, b) {
     if (!isNumber(a) || !isNumber(b)) {
         return null;
     }
-    if (b === 0) {
+    if (b == 0) {
         return null;
     }
     return a / b;
